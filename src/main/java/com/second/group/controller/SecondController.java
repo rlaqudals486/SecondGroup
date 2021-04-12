dfb521b27831f8a8d0d58e534328d73fbfe4d5de
rlaqudals486 <rlaqudals486@gmail.com>
2021-04-12 13:49:09 +0900

    2021-04-12 변경사항

--- src/main/java/com/second/group/controller/SecondController.java
+++ src/main/java/com/second/group/controller/SecondController.java
@@ -1,17 +1,38 @@
 package com.second.group.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.second.group.dto.SecondRecipeDto;
+import com.second.group.service.SecondService;
 
 @Controller
 public class SecondController {
-	@RequestMapping("/second/SecondBoardList")
+	
+	@Autowired
+	SecondService secondService;
+	
+	@RequestMapping(value="/second/mypage", method=RequestMethod.PUT)
 	public String SecondBoardList() throws Exception {
-		return "/second/SecondBoardList";
+		return "/second/mypage";
+	}
+	
+	@RequestMapping(value="/second/myBoard", method=RequestMethod.GET)
+	public ModelAndView SelectSecondList() throws Exception {
+		ModelAndView mv = new ModelAndView("/second/myBoard");
+		List<SecondRecipeDto> list = secondService.selectSecondList();
+		mv.addObject("data", list);
+		return mv;
 	}
 	
 	@RequestMapping("/second/recipeDetailed")
 	public String SecondDetail() throws Exception {
 		return "/second/recipeDetailed";
 	}
+	
 }
---
+++ src/main/java/com/second/group/service/SecondServiceImpl.java
@@ -0,0 +1,22 @@
+package com.second.group.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.second.group.dto.SecondRecipeDto;
+import com.second.group.mapper.SecondMapper;
+
+@Service
+public class SecondServiceImpl implements SecondService {
+	
+	@Autowired
+	SecondMapper secondMapper;
+	
+	@Override
+	public List<SecondRecipeDto> selectSecondList() throws Exception {
+		return secondMapper.selectSecondList();
+	}
+
+}
