a1a1a338ceb0bf3d662886b41f1ca4f7cd344602
kk118 <oper366@gmail.com>
2021-04-13 16:49:22 +0900

    test7

--- src/main/java/com/second/group/controller/SecondController.java
+++ src/main/java/com/second/group/controller/SecondController.java
@@ -9,33 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.second.group.dto.SecondCommentDto;
 import com.second.group.dto.SecondRecipeDto;
 import com.second.group.service.SecondService;
-import com.second.group.dto.SecondCommentDto;
 
 @Controller
 public class SecondController {
-
 	@Autowired
 	private SecondService secondService;
 	
-	@RequestMapping(value="/second/mypage", method=RequestMethod.GET)
+	
+	@RequestMapping("/second/SecondBoardList")
 	public String SecondBoardList() throws Exception {
-		return "/second/mypage";
+		return "/second/SecondBoardList";
 	}
 	
-	@RequestMapping(value="/second/myBoard", method=RequestMethod.GET)
-	public ModelAndView SelectSecondList() throws Exception {
-		ModelAndView mv = new ModelAndView("/second/myBoard");
-		List<SecondRecipeDto> list = secondService.selectSecondList();
-		mv.addObject("data", list);
-		return mv;
-	}
-	
-	@RequestMapping("/second/recipeDetailed")
-	public String SecondDetail() throws Exception {
-		return "/second/recipeDetailed";
-	}
 	
 	@RequestMapping(value="/second", method=RequestMethod.GET)
 	public ModelAndView HomeList() throws Exception{
@@ -51,6 +39,7 @@ public class SecondController {
 		SecondRecipeDto item2 = new SecondRecipeDto();
 		SecondRecipeDto item3 = new SecondRecipeDto();
 		SecondRecipeDto item4 = new SecondRecipeDto();
+			
 		item1.setRecipeFilePath("/img/001.jpg");
 		item2.setRecipeFilePath("/img/002.jpg");
 		item3.setRecipeFilePath("/img/003.jpg");
@@ -64,37 +53,6 @@ public class SecondController {
 		mv.addObject("comment", comment);
 		mv.addObject("datas", list);
 		return mv;
-
-	@RequestMapping(value="/second/SecondLogin", method=RequestMethod.GET)
-	public String SecondLogin() throws Exception {
-		return "/second/SecondLogin";
-	}
-	
-	@RequestMapping(value="/second/SecondJoin", method=RequestMethod.GET)
-	public String SecondJoin() throws Exception {
-		return "/second/SecondJoin";
-
 	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
