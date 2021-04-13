c61dd4344f37ab2e8db057e58caadba5a64d4caf
kk118 <oper366@gmail.com>
2021-04-13 16:45:47 +0900

    test6

--- src/main/java/com/second/group/controller/SecondController.java
+++ src/main/java/com/second/group/controller/SecondController.java
@@ -1,5 +1,6 @@
 package com.second.group.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +29,25 @@ public class SecondController {
 	public ModelAndView HomeList() throws Exception{
 		ModelAndView mv = new ModelAndView("/second/Home");
 		
-		List<SecondRecipeDto> list = secondService.selectSecondHomeList();
+//		List<SecondRecipeDto> list = secondService.selectSecondHomeList();
 		List<SecondCommentDto> comment = secondService.selectCommentHomeList();
+		
+		
+		//images test용
+		List<SecondRecipeDto> list = new ArrayList<SecondRecipeDto>();
+		SecondRecipeDto item1 = new SecondRecipeDto();
+		SecondRecipeDto item2 = new SecondRecipeDto();
+		SecondRecipeDto item3 = new SecondRecipeDto();
+		SecondRecipeDto item4 = new SecondRecipeDto();
+		item1.setRecipeFilePath("/img/001.jpg");
+		item2.setRecipeFilePath("/img/002.jpg");
+		item3.setRecipeFilePath("/img/003.jpg");
+		item4.setRecipeFilePath("/img/004.jpg");
+		list.add(item1);
+		list.add(item2);
+		list.add(item3);
+		list.add(item4);
+		
 		
 		mv.addObject("comment", comment);
 		mv.addObject("datas", list);
